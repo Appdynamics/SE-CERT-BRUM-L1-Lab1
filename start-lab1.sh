ae77fb575938b5b820d51c74126cd7fc3e5392f8
Derrek Young <derrek@derrekyoung.com>
2017-09-29 12:34:17 -0400

    removed the git clone

--- start-lab1.sh
+++ start-lab1.sh
@@ -4,12 +4,6 @@
 LAB_NAME="SE-CERT-BRUM-L1-Lab1"
 LAB_DIR="/home/ubuntu/$LAB_NAME/"
 
-if [ ! -d "$LAB_DIR" ]; then
-  # Enter here if $LAB_DIR doesn't exist.
-  echo "Cloning lab source from git"
-  git clone git://github.com/Appdynamics/$LAB_NAME.git
-fi
-
 cd $LAB_DIR
 
 echo "Updating lab source from git"
