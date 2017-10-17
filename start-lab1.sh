1b7b6583f62d208658ca4c460812a286fa15fe6b
Derrek Young <derrek@derrekyoung.com>
2017-10-17 14:48:42 -0400

    Kill then install

--- start-lab1.sh
+++ start-lab1.sh
@@ -9,11 +9,11 @@ cd $LAB_DIR
 echo "Updating lab source from git"
 git pull
 
-echo "Installing/updating packages"
-npm install
-
 echo "Killing running lab instances of $LAB_NAME"
 pkill -f "index.js"
+
+echo "Installing/updating packages"
+npm install
 
 echo "Starting the lab"
 nohup npm start > /tmp/$LAB_NAME.log 2>&1 &
